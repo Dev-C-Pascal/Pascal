128c38c20bf5a252b1675df8ca86607bdb31f6e8
Dev-C-Pascal <lesnikovevgen7965@gmail.com>
2021-03-07 14:12:03 +0200

    Create Ищет количество положительных элемнетов массива.pas
    Ищет количество положительных элементов массива

---
+++ Ищет количество положительных элемнетов массива.pas
@@ -0,0 +1,13 @@
+﻿var a: array [-100..100] of integer; 
+    i, n, k: integer;
+begin
+  Read(n);
+  i:=0;
+  k:=0;
+  for i := 0 to n-1 do begin 
+    read(a[i]);
+    if a[i] > 0 then k:=k+1;
+  
+  end;
+  writeln(k);
+end.
