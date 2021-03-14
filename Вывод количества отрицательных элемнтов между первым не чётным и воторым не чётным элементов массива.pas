32cab90733ab3a90cac41a0d296403e87db501f1
Dev-C-Pascal <lesnikovevgen7965@gmail.com>
2021-03-14 15:25:29 +0200

    Create Вывод количества отрицательных элемнтов между первым не чётным и воторым не чётным элементов массива.pas
    Вывод количества отрицательных элементов между первым не чётным и вторым не чётным элементов массива

---
+++ Вывод количества отрицательных элемнтов между первым не чётным и воторым не чётным элементов массива.pas
@@ -0,0 +1,34 @@
+﻿var
+    arr: array[1..100] of integer;
+    i, min, max: byte;
+    s, n: integer;
+begin
+  read(n);
+  for i := 1 to N do begin
+      read(arr[i]);
+  end;  
+  //min := 1;
+  //max := 1;
+  //for i := 2 to N do begin
+  for i := 1 to N do begin
+      //if arr[i] < arr[min] then min := i else
+      //if arr[i] > arr[max] then max := i;
+      if (arr[i] mod 2 <> 0) and (min = 0) then min := i
+      else if (arr[i] mod 2 <> 0) and (max = 0) and (min > 0) then max := i
+      else if (min > 0) and (max > 0) then break;
+  end;
+  
+  {if min > max then begin
+      i := min;
+      min := max;
+      max := i;
+  end;}
+  
+  s := 0;
+  if (min > 0) and (max > 0) then begin
+    for i:=min to max do begin
+        if (arr[i] < 0) then s := s + 1;
+    end;
+  end;
+  writeln(s);
+end.
