91c6dbc925109f5a33f92b2c125d2a6c7e24f095
Dev-C-Pascal <lesnikovevgen7965@gmail.com>
2021-03-20 10:51:55 +0200

    Вывод количества не повторяющихся элементов массива.

---
+++ Вывод количества не повторяющихся элементов массива.pas
@@ -0,0 +1,18 @@
+﻿type
+  TMass = array of Integer;
+var
+  N, I, s : Integer; 
+  A : TMass;
+  
+begin
+  s := 1;
+
+  Read(N);
+  SetLength(A, N);
+  for I := 0 to N - 1 do 
+    Read(A[I]);
+  for I := 1 to N - 1 do begin
+      if (a[i-1] < a[i]) then s:=s+1;
+  end;
+  writeln(s);
+end.
