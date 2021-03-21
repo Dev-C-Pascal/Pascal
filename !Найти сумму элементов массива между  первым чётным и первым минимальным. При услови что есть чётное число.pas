6caa4e2b9dc3c357ca342d153b6994dfc1c50cc1
Dev-C-Pascal <lesnikovevgen7965@gmail.com>
2021-03-21 10:40:13 +0200

    Найти сумму элементов массива между  первым чётным и первым минимальным. При условии что есть чётное число

---
+++ !Найти сумму элементов массива между  первым чётным и первым минимальным. При услови что есть чётное число.pas
@@ -0,0 +1,33 @@
+﻿type
+  TMass = array of Int64;
+var
+  N, I : Int64; 
+  A : TMass;
+  Sum, Max, Min : Int64;
+begin
+  Read(N);
+  SetLength(A, N);
+  Sum := 0;
+  for I := 0 to N - 1 do 
+    Read(A[I]);
+  Max := -1; 
+  Min := 0;
+  for I := 0 to N - 1 do
+  begin
+    if (A[I] < A[min]) then Min := I;
+    if (A[I] mod 2 = 0) and (max = -1) then Max := I;
+  end; 
+  if (Min < Max) then begin
+  for i := min to max do
+    begin
+        Sum := Sum + a[i];
+    end;
+  end
+  else begin
+    for i := max to min do
+    begin
+        Sum := Sum + a[i];
+    end;
+  end;
+  writeln (Sum);
+end.
