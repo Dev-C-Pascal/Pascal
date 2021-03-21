c04a8e368d6dc3b9dbd9de46da1d284a1f011d9e
Dev-C-Pascal <lesnikovevgen7965@gmail.com>
2021-03-21 10:50:53 +0200

    Найти сумму между отрицательных элементов массива. Если отрицательных нет то посчитать сумму массива.

---
+++ Найти сумму между отрицательных элементов массива.Если отрицательных нет то посчитать сумму массива..pas
@@ -0,0 +1,37 @@
+﻿type
+  TMass = array of Int64;
+var
+  N, I : Int64; 
+  A : TMass;
+  Sum, Max, Min : Int64;
+begin
+  Read(N);
+  SetLength(A, N);
+  Sum := 0;
+  for I := 0 to N - 1 do 
+    Read(A[I]);
+  Max := -1; 
+  Min := -1;
+  for I := 0 to N - 1 do
+  begin
+    if (A[I] < 0) and (Min = -1) then Min := I;
+    if (A[I] < 0) then Max := I;
+  end; 
+  if (Min = Max) then begin
+    Min := 0;
+    Max := N - 1;
+  end;
+  if (Min < Max) then begin
+  for i := min to max do
+    begin
+        Sum := Sum + a[i];
+    end;
+  end
+  else begin
+    for i := max to min do
+    begin
+        Sum := Sum + a[i];
+    end;
+  end;
+  writeln (Sum);
+end.
