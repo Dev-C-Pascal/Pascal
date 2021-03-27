ad0bee6b608c129988d5228c0a7c186fb5f12e8a
Dev-C-Pascal <lesnikovevgen7965@gmail.com>
2021-03-27 11:41:13 +0200

    Найти сумму между первым минимальным элементом и последним максимальным элемнтом включительно.

---
+++ !! Найти сумму между первым инимальным элементом и последним максимальным элемнтом включительно.pas
@@ -0,0 +1,34 @@
+﻿type
+  TMass = array of Int64;
+var
+  N, I : Int64; 
+  A : TMass;
+  Sum, Max, Min : Int64;
+begin
+  Read(N);
+  SetLength(A, N);
+  Sum := 0;
+  for I := 0 to N - 1 do 
+    Read(A[I]);
+  Max := 0; 
+  Min := 0;
+  for I := 0 to N - 1 do
+  begin
+    if A[I] < A[Min] then Min := I;
+    if A[I] >= A[Max] then Max := I;
+  end; 
+  if (Min < Max) then begin
+    for i:=min to max do
+      begin
+        Sum := Sum + a[i];
+      end;
+   end
+  else begin
+    for i:=max to min do
+      begin
+        Sum := Sum + a[i];
+      end;
+      
+  end;
+  writeln (Sum);
+end.
