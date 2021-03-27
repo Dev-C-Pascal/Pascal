1ac16f9df409b07ff2f31c145da3291901fa6bed
Dev-C-Pascal <lesnikovevgen7965@gmail.com>
2021-03-27 15:50:38 +0200

    Create Работа с файлами.pas

---
+++ Работа с файлами.pas
@@ -0,0 +1,21 @@
+﻿var
+    f: file of char;
+    c: char;
+    i, n: byte;
+ 
+begin
+    assign (f, 'D:\file.txt');
+    rewrite (f);
+ 
+    write ('Количество символов: ');
+    readln (n);
+ 
+    for i:=1 to n do begin
+        write ('Введите символ: ');
+        readln (c);
+        write (f, c);
+    end;
+ 
+    close (f);
+ 
+end.
